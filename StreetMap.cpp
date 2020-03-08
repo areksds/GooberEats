846f4f712fa020f24c2234e56891b2e73faad8d2
Arek Der-Sarkissian <areksds@gmail.com>
2020-03-08 16:34:30 -0700

    Working StreetMap

--- StreetMap.cpp
+++ StreetMap.cpp
@@ -46,41 +46,63 @@ bool StreetMapImpl::load(string mapFile)
     {
         vector<StreetSegment> segs;
         // Street name
-        istringstream str(line);
-        str >> street;
+        street = line;
         // Number of segment pairs
         getline(inf,line);
         istringstream num(line);
         num >> segments;
         string lat1, long1, lat2, long2;
-        GeoCoord key;
-        for (int i = 0; i <= segments; i++)
+        for (int i = 0; i < segments; i++)
         {
-            if (i > 0)
+            // Get new segments
+            getline(inf,line);
+            istringstream seg(line);
+            // Segments
+            seg >> lat1 >> long1;
+            GeoCoord coord1(lat1, long1);
+            seg >> lat2 >> long2;
+            GeoCoord coord2(lat2, long2);
+            // Normal segment
+            StreetSegment first(coord1,coord2,street);
+            vector<StreetSegment>* exist1 = m_map.find(coord1);
+            if (exist1 == nullptr)
             {
-                // Reverse segments
-                GeoCoord coord1(lat2, long2);
-                GeoCoord coord2(lat1, long1);
-                StreetSegment segment(coord1,coord2,street);
-                segs.push_back(segment);
-                if (i == segments)
-                    key = GeoCoord(lat2,long2);
+                vector<StreetSegment> segs;
+                segs.push_back(first);
+                m_map.associate(coord1,segs);
             }
-            if (i < segments)
+            else
             {
-                // Get new segments
-                getline(inf,line);
-                istringstream seg(line);
-                // Segments
-                seg >> lat1 >> long1;
-                GeoCoord coord1(lat1, long1);
-                seg >> lat2 >> long2;
-                GeoCoord coord2(lat2, long2);
-                StreetSegment segment(coord1,coord2,street);
-                segs.push_back(segment);
-                key = GeoCoord(lat1,long2);
+                auto it = exist1->begin();
+                for (; it != exist1->end(); it++)
+                {
+                    if (*it == first)
+                        break;
+                }
+                if (it == exist1->end())
+                    exist1->push_back(first);
             }
-            m_map.associate(key, segs);
+            // Reverse segment
+            StreetSegment reverse(coord2,coord1,street);
+            vector<StreetSegment>* exist2 = m_map.find(coord2);
+            if (exist2 == nullptr)
+            {
+                vector<StreetSegment> segs;
+                segs.push_back(reverse);
+                m_map.associate(coord2,segs);
+            }
+            else
+            {
+                auto it = exist2->begin();
+                for (; it != exist2->end(); it++)
+                {
+                    if (*it == reverse)
+                        break;
+                }
+                if (it == exist2->end())
+                    exist2->push_back(first);
+            }
+            
         }
     }
     return true;
@@ -88,7 +110,11 @@ bool StreetMapImpl::load(string mapFile)
 
 bool StreetMapImpl::getSegmentsThatStartWith(const GeoCoord& gc, vector<StreetSegment>& segs) const
 {
-    return false;  // Delete this line and implement this function correctly
+    const vector<StreetSegment>* search = m_map.find(gc);
+    if (search == nullptr)
+        return false;
+    segs = *search;
+    return true;
 }
 
 //******************** StreetMap functions ************************************
